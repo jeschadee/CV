12205ed3b0aec070ff5c29db1995afd0d02f8d78
Leonardo <llarrosa@digitalhouse.com>
2020-03-20 13:04:44 -0300

    First commit

---
+++ precache-manifest.0bc26a361f842b9e0e492ec843b1ed6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dd8f3e5a444fd8b44793880d1ddacf5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3089454475a129c91cda",
+    "url": "/static/js/2.402c5253.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.402c5253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f51b8c82d335ad62edf6",
+    "url": "/static/js/main.ac982fea.chunk.js"
+  },
+  {
+    "revision": "ac8de8676281b9671291",
+    "url": "/static/js/runtime-main.64ea6a7f.js"
+  }
+]);
